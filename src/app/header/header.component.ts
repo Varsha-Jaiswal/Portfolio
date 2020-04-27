ea9db5ea3816841cddb55512eb7a7f681b34f287
varsha-jaiswal <shaluvarshajaiswal@gmail.com>
2020-04-27 20:36:31 +0530

    minor update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  isGallerySelected = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectGallery() {
+    this.isGallerySelected = true;
   }
 
 }
