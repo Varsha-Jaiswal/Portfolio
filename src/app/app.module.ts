187bda71514905f78d15aa7b11cc1ac8d0c33f57
varsha-jaiswal <shaluvarshajaiswal@gmail.com>
2020-04-28 14:03:27 +0530

    PWA initial + Update snackbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSnackBarModule, MatSnackBar } from '@angular/material';
+import { ServiceWorkerModule, SwUpdate } from '@angular/service-worker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ProjectComponent } from './project/project.component';
@@ -12,6 +15,7 @@ import { HomeDataComponent } from './home-data/home-data.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { ScrollToTopComponent } from './scroll-to-top/scroll-to-top.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -28,9 +32,22 @@ import { ScrollToTopComponent } from './scroll-to-top/scroll-to-top.component';
     ScrollToTopComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSnackBarModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(update: SwUpdate, snackBar: MatSnackBar) {
+    update.available.subscribe(update => {
+      // Aloow refresh from snackbar
+      const snack = snackBar.open('Update Available', 'Reload');
+      snack.onAction().subscribe(() => {
+        window.location.reload();
+      });
+    });
+  }
+}
